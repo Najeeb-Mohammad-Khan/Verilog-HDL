9568847cac6dc7508338721cf9176bdeeadf05eb
Najeeb Mohammad Khan <67659898+Najeeb-Mohammad-Khan@users.noreply.github.com>
2020-07-02 17:30:46 +0530

    Verilog Code for AND GATE

---
+++ AND_GATE/AND_GATE_Test_Fixture.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+
+module AND_GATE_Test_Fixture;
+
+	// Inputs
+	reg A;
+	reg B;
+
+	// Outputs
+	wire Y;
+
+	// Instantiate the Unit Under Test (UUT)
+	AND_GATE uut (
+		.Y(Y), 
+		.A(A), 
+		.B(B)
+	);
+
+	initial begin
+		// Initialize Inputs
+		A = 0;
+		B = 0;
+		#100;  // Wait for 100ns.
+        
+		A = 1;
+		#100;  // Wait for 100ns.
+		
+		A = 0;
+		B = 1;
+		#100;  // Wait for 100ns.
+
+		A = 1;
+		#100;  // Wait for 100ns.
+
+	end
+      
+endmodule
+
