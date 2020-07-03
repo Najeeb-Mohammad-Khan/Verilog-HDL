a05d536a543eade6f05f29a81158270f22fb9e45
Najeeb Mohammad Khan <67659898+Najeeb-Mohammad-Khan@users.noreply.github.com>
2020-07-03 12:47:18 +0530

    Delete NOT_GATE.v

--- NOT GATE/NOT_GATE.v
+++
@@ -1,7 +0,0 @@
-`timescale 1ns / 1ps
-
-module NOT_GATE(output Y,input A);   ////Declaring NOT Gate Module with Y as OUTPUT and A as INPUT.
-
-not F1(Y,A);   // Using inbuilt 'not' gate.
-
-endmodule
