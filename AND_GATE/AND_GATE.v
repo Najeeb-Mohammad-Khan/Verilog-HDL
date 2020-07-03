81b2a445fc5e766d737d35819c2bb1876bacc81e
Najeeb Mohammad Khan <67659898+Najeeb-Mohammad-Khan@users.noreply.github.com>
2020-07-03 12:45:00 +0530

    Delete AND_GATE.v

--- AND_GATE/AND_GATE.v
+++
@@ -1,7 +0,0 @@
-`timescale 1ns / 1ps
-
-module AND_GATE(output Y, input A,B); //Declaring AND Gate Module with Y as OUTPUT and A,B as INPUT.
-
-and F1(Y,A,B);                        //Using inbuilt 'and' gate.
-
-endmodule
